466fb698dd10ffa5a79ee601578c33a74237ccea
kooltran <kiettrankm11@gmail.com>
2020-05-25 01:18:29 +0700

    Updates

---
+++ precache-manifest.50ea837be8df166c3cedb8aa3c036af6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0fc7870d114aefd8f3bbca4036c81b",
+    "url": "/kiet-autocomplete-assignment/index.html"
+  },
+  {
+    "revision": "a2deb9c95deea8de58d3",
+    "url": "/kiet-autocomplete-assignment/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "f0b804e00270c53c70b3",
+    "url": "/kiet-autocomplete-assignment/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2deb9c95deea8de58d3",
+    "url": "/kiet-autocomplete-assignment/static/js/2.f2f7e792.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kiet-autocomplete-assignment/static/js/2.f2f7e792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b804e00270c53c70b3",
+    "url": "/kiet-autocomplete-assignment/static/js/main.1ad21ac9.chunk.js"
+  },
+  {
+    "revision": "8230531247363b1293f7",
+    "url": "/kiet-autocomplete-assignment/static/js/runtime-main.c7a8c1c4.js"
+  },
+  {
+    "revision": "7bb1a2fa272288176c638af7825d801c",
+    "url": "/kiet-autocomplete-assignment/static/media/day.7bb1a2fa.jpg"
+  },
+  {
+    "revision": "6f1f85f3a1c1a07ace28f724b77de4c2",
+    "url": "/kiet-autocomplete-assignment/static/media/moon.6f1f85f3.svg"
+  },
+  {
+    "revision": "0aee7d6d49cb5b5dcce8beb1b1571712",
+    "url": "/kiet-autocomplete-assignment/static/media/night.0aee7d6d.jpg"
+  },
+  {
+    "revision": "aa59b8a4563d9e4bab5397881af52dcd",
+    "url": "/kiet-autocomplete-assignment/static/media/sun.aa59b8a4.svg"
+  }
+]);
